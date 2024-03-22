8e4d13111ae8a8df285cfacc44e845d78a1cb178
danandersondeveloper <dananderson.developer@gmail.com>
2024-03-22 16:26:52 +0200

    Create header component

---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+// Components
+import Logo from "./Logo";
+
+const Header = () => {
+	return(
+		<header id="header" className="header">
+			<div className="container">
+				<Logo />
+			</div>
+		</header>
+	)
+}
+
+export default Header;
